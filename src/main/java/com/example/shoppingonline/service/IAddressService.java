a456a796f279b5948068152e46b83de107905db4
dongduongngo <0966856230q@gmail.com>
2021-10-04 16:43:40 +0700

    adding service

--- src/main/java/com/example/shoppingonline/service/IAddressService.java
+++ src/main/java/com/example/shoppingonline/service/IAddressService.java
@@ -1,4 +1,16 @@
 package com.example.shoppingonline.service;
 
+import com.example.shoppingonline.DTO.AddressDto;
+import com.example.shoppingonline.entity.Address;
+
+import java.util.List;
+
 public interface IAddressService {
+    List<Address> getAllAddress();
+    List<AddressDto> getAllAddressDto();
+
+    AddressDto create(AddressDto addressDto);
+    AddressDto update(AddressDto addressDto);
+
+    void delete(Long id);
 }
