2039147c824e7c6f4a1da1c336718b444a077e4b
dongduongngo <0966856230q@gmail.com>
2021-10-04 15:42:22 +0700

    create addresdto

---
+++ src/main/java/com/example/shoppingonline/DTO/AddressDto.java
@@ -0,0 +1,11 @@
+package com.example.shoppingonline.DTO;
+
+import lombok.Data;
+
+@Data
+public class AddressDto {
+    private Long addressId;
+    private Long userId;
+    private String name;
+    private String address;
+}
